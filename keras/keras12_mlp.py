5ef6ea01d71f5b7c6a98ba87b1255992f29cd5ef
MJK0211 <kimminjong0211@gmail.com>
2020-11-10 18:03:20 +0900

    11_10_commit

--- keras/keras12_mlp.py
+++ keras/keras12_mlp.py
@@ -1,12 +1,14 @@
 #1. 데이터
 import numpy as np
+
 x = np.array([range(1,101), range(711,811), range(100)]) #100개의 데이터 3개 - 100행 3열
 y = np.array((range(101,201), range(311,411), range(100)))
 
 print(x)
+print(x.shape) #(3,100)
+
+# 과제(100,3)으로 바꿔보기
 x = x.T
+
 print(x)
 print(x.shape)
-
-# 과제(100,3)
-
